b401135a2a7b3a6aea2bb69883e90859645dd8ee
Samuelisch <samuelchanwk@hotmail.com>
2021-06-14 19:33:49 +0800

    created IIFE for display for player name and symbol for main page

--- js/script.js
+++ js/script.js
@@ -9,11 +9,18 @@ const playerTwoSymbols = playerTwoNode.querySelector('.symbols');
 
 const submitBtn = document.getElementById('form-submit');
 
-//selectors for main page
-const firstNameDisplay = document.querySelector('.first-name');
-const firstSymbolDisplay = document.querySelector('.first-symbol-display');
-const secondNameDisplay = document.querySelector('.second-name');
-const secondSymbolDisplay = document.querySelector('.second-symbol-display');
+//IIFE for main page display
+const displayNameSymbol = (playerOne, playerTwo) => (function(playerOne, playerTwo) {
+    const firstNameDisplay = document.querySelector('.first-name');
+    const firstSymbolDisplay = document.querySelector('.first-symbol-display');
+    const secondNameDisplay = document.querySelector('.second-name');
+    const secondSymbolDisplay = document.querySelector('.second-symbol-display');
+
+    firstNameDisplay.textContent = playerOne.name;
+    firstSymbolDisplay.appendChild(playerOne.actionSymbol);
+    secondNameDisplay.textContent = playerTwo.name;
+    secondSymbolDisplay.appendChild(playerTwo.actionSymbol);
+})(playerOne, playerTwo);
 
 //selectors for main page
 const configureBtn = document.getElementById('configure');
@@ -91,10 +98,7 @@ function displayForm() {
     const playerOne = playerFactory(playerOneName.value, true, undefined, playerOneNode.querySelector('.symbol-toggle').firstElementChild);
     const playerTwo = playerFactory(playerTwoName.value, playerTwoNode.querySelector('input[name="is-human-2"]:checked').id == "human", playerTwoNode.querySelector('input[name="difficulty"]:checked').value, playerTwoNode.querySelector('.symbol-toggle').firstElementChild);
 
-    firstNameDisplay.textContent = playerOne.name;
-    firstSymbolDisplay.appendChild(playerOne.actionSymbol);
-    secondNameDisplay.textContent = playerTwo.name;
-    secondNameDisplay.appendChild(playerTwo.actionSymbol);
+    displayNameSymbol(playerOne, playerTwo);
 
     console.log(playerOne, playerTwo);
 }
@@ -107,6 +111,11 @@ const playerFactory = (name, isHuman, difficulty, actionSymbol) => {
 
     return {name, isHuman, difficulty, actionSymbol};
 };
+
+//create IIFE for game grid
+const gameGrid = () => {
+
+}
 
 /*test for click event
 window.addEventListener('click', clicked);
