9d84b4c39c9992a41ffbbea9c299a2adf188f929
Samuelisch <samuelchanwk@hotmail.com>
2021-06-16 13:27:28 +0800

    initialized module and function factory for game, gameGrid and Player.

---
+++ js/script.js
@@ -0,0 +1,54 @@
+const createPlayer = ({name, symbol}) => ({
+    name,
+    symbol,
+    array: [],
+
+    setUserName(name) {
+        this.name = name;
+    },
+
+    setSymbol(symbol) {
+        this.symbol = symbol;
+    },
+
+    win() {
+        return `${this.name} wins!`;
+    },
+
+    action(x) {
+      this.array.push(x);
+    }
+});
+
+//module to create, reset and fill the grid for the game
+const gameBoard = (function() {
+
+    const start = function() {
+        console.log(`Game ${num} start!`);
+        //manipulate DOM
+    }
+
+    const reset = function() {
+        console.log('Resetting game...');
+        //manipulate DOM
+    }
+
+    //check if winning condition reached
+
+    //if all squares filled and no winning condition, declare tie
+
+    return{start, reset};
+})();
+
+//game Module to create players, and have game flow.
+const game = (() => {
+    let player1 = createPlayer({name: userName, symbol: symbol});
+
+    //set flags for player turns
+
+    //set action functions for each click event
+
+    //check if winning condition reached (winning condition in array)
+    
+})();
+
