ca8bc53ad453fa76ca170a7138ee578a9ea6595d
Samuelisch <samuelchanwk@hotmail.com>
2021-06-21 18:37:23 +0800

    added winning conditions and events

--- js/script.js
+++ js/script.js
@@ -48,17 +48,58 @@ const game = (() => {
     }
 
     //create winning combinations
+    const win = 
+    [
+        [0, 1, 2],
+        [0, 3, 6],
+        [0, 4, 8],
+        [1, 4, 7],
+        [2, 5, 8],
+        [2, 4, 6],
+        [3, 4, 5],
+        [6, 7, 8]
+    ];
+
+    function won(player) {
+        const array = [...player.array];
+        console.log(array);
+        for (let i = 0; i < win.length; i++) {
+            if (win[i].every(num => array.includes(num))) {
+                return true;
+            }
+            return false;
+        }
+    }
 
     //set action functions for each click event / player action to array (player.action(cellnum)
     function clicked(e) {
         const cell = e.target;
 
+        if (cell.textContent) {
+            console.log('cell is filled');
+            return; // returns if clicked on filled cell
+        }
+        //checks player flag for symbol input, adds cell id into player array
         if (playerOneFlag) {
             cell.textContent = player1.symbol;
+            player1.array.push(parseInt(cell.id));
+            console.log(player1.array);
+            if (won(player1)) {
+                console.log('player1 won!')
+                return;
+            }
         } else {
             cell.textContent = player2.symbol;
+            player2.array.push(parseInt(cell.id));
+            console.log(player2.array);
+            if (won(player2)) {
+                console.log('player2 won!')
+                return;
+            }
         }
-
+        //checks for winning condition
+        
+        //function for next turn
         nextPlayerTurn();
     }
 
