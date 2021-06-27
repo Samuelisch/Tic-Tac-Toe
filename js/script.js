1baf67ca0f1a489e1589d2d1ff0e5aed9f8a6ce1
Samuelisch <samuelchanwk@hotmail.com>
2021-06-27 20:30:31 +0800

    added simple bot behaviour with randomly generated number - have to debug certain functions to get better functionality and bot behaviour

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
 const text = document.querySelector('.text');
-const gameBox = document.querySelector('.game-box');
-const submitBtn = document.querySelector('.submit-btn');
-const configBtn = document.querySelector('.configure-btn');
-const form = document.querySelector('form');
+let gameEnd = false;
 
 const createPlayer = ({name, symbol}) => ({
     name,
@@ -26,14 +23,44 @@ const createPlayer = ({name, symbol}) => ({
     }
 });
 
+const formBoard = (() => {
+    const form = document.querySelector('form');
+    const configBtn = document.querySelector('.configure-btn');
+    const submitBtn = document.querySelector('.submit-btn');
+
+    function formSubmit(e) {
+        e.preventDefault();
+        form.classList.toggle('display-form');
+        const playerOneName = document.getElementById('name1').value;
+        const playerTwoName = document.getElementById('name2').value;
+        game.player1.name = playerOneName;
+        game.player2.name = playerTwoName;
+        gameBoard.reset();
+    }
+
+    function dropForm() {
+        form.classList.toggle('display-form');
+    }
+
+    submitBtn.addEventListener('click', formSubmit);
+    configBtn.addEventListener('click', dropForm);
+})();
+
 //game Module to create players, and have game flow.
 const game = (() => {
+    const botDifficulty = document.querySelector('input[name="difficulty"]:checked').value //easy, medium or hard
+
+    function random(arr) {
+        const index = Math.floor(Math.random() * arr.length);
+        return arr[index];
+    }
+
     let player1 = createPlayer({name: 'Player1', symbol: 'X'});
     let player2 = createPlayer({name: 'Player2', symbol: 'O'});
 
     //set flags for player turns
-    playerOneFlag = true;
-    gameEnd = false;
+    let playerOneFlag = true;
+    
 
     function turn(player) {
         text.textContent = `${player.name}'s turn!`
@@ -44,6 +71,9 @@ const game = (() => {
         if (playerOneFlag) {
             playerOneFlag = !playerOneFlag;
             turn(player2);
+            if (document.querySelector('#ai').checked) {
+                botTurn();
+            }
         } else {
             playerOneFlag = !playerOneFlag;
             turn(player1);
@@ -53,6 +83,7 @@ const game = (() => {
     //set action functions for each click event / player action to array (player.action(cellnum)
     function clicked(e) {
         const cell = e.target;
+        const index = gameBoard.gameArray.indexOf(parseInt(cell.id));
 
         //ignore if game is won or clicked on filled cell
         if (gameEnd || cell.textContent) {
@@ -66,10 +97,15 @@ const game = (() => {
         //adds player symbol into grid cell, pushes cell id into array, and checks for win
         cell.textContent = player.symbol;
         player.action(parseInt(cell.id));
+        //removes cell from gameBoard array, for bot usage (splice, index, 1)
+        console.log(`removed ${parseInt(cell.id)} from array!`);
+        gameBoard.gameArray.splice(index, 1);
+        console.log(player.array);
         //checks for winning condition
         gameBoard.isWon(player);
-        gameBoard.isTie(player);
+        gameBoard.isTie();
         if (gameEnd) {
+            console.log('game ending');
             return;
         }
         
@@ -77,12 +113,35 @@ const game = (() => {
         nextPlayerTurn();
     }
 
-    return {player1, player2, clicked, gameEnd, turn}
+    function botTurn() {
+        const player = player2;
+        const cell = document.querySelector(`.cell[id="${random(gameBoard.gameArray)}"]`);
+        const index = gameBoard.gameArray.indexOf(parseInt(cell.id));
+
+        cell.textContent = player.symbol;
+        player.action(parseInt(cell.id));
+        console.log(`removed ${parseInt(cell.id)} from array!`);
+        gameBoard.gameArray.splice(index, 1);
+
+        gameBoard.isWon(player);
+        gameBoard.isTie();
+        if (gameEnd) {
+            return;
+        }
+        
+        //function for next turn
+        console.log(`array is ${gameBoard.gameArray}`);
+        nextPlayerTurn();
+    }
+
+    return {player1, player2, clicked, turn}
 })();
 
 //module to create, reset and fill the grid for the game
 const gameBoard = (() => {
+    const gameBox = document.querySelector('.game-box');
     const restart = document.querySelector('.restart-btn');
+    let gameArray = [0, 1, 2, 3, 4, 5, 6, 7, 8];
     
     //function to start game
     function start() {
@@ -117,8 +176,10 @@ const gameBoard = (() => {
 
     //check if winning condition reached
     function isWon(player) {
+        console.log('checking win');
         for (let i = 0; i < win.length; i++) {
             if (win[i].every(num => player.array.includes(num))) {
+                console.log('win!');
                 text.textContent = `${player.win()}`;
                 gameEnd = true;
             }
@@ -136,24 +197,7 @@ const gameBoard = (() => {
 
     restart.addEventListener('click', reset);
 
-    return{start, reset, isWon, isTie};
+    return{start, reset, isWon, isTie, gameArray};
 })();
 
-function formSubmit(e) {
-    e.preventDefault();
-    form.classList.toggle('display-form');
-    const playerOneName = document.getElementById('name1').value;
-    const playerTwoName = document.getElementById('name2').value;
-    game.player1.name = playerOneName;
-    game.player2.name = playerTwoName;
-    gameBoard.reset();
-}
-
-function dropForm() {
-    form.classList.toggle('display-form');
-}
-
 gameBoard.start();
-
-submitBtn.addEventListener('click', formSubmit);
-configBtn.addEventListener('click', dropForm);
